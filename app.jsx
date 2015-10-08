fc89e45f5b3a487eeed092032d5de8afa16f9761
Caleb Thill <calebnthill@gmail.com>
2015-10-08 14:39:20 -0400

    Added node_modules and basic react.js structure.

---
+++ app.jsx
@@ -0,0 +1,7 @@
+var React = require('react')
+var TodoApp = require('./components/todo_app')
+
+React.render(
+  <TodoApp />,
+  document.getElementById('content')
+);
---
+++ components/todo_app.jsx
@@ -0,0 +1,10 @@
+var React = require('react')
+
+var TodoApp = React.createClass({
+  displayName: 'HelloReact',
+  render: function(){
+    return <div>Hello React</div>
+  }
+});
+
+module.exports = TodoApp;
